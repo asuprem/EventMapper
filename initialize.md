e54779e8c89d5fc21afe5b5d8c55986654b9f1f1
Abhijit Suprem <asf2182@columbia.edu>
2019-01-19 18:08:27 -0500

    added mysql init

--- initialize.md
+++ initialize.md
@@ -3,3 +3,8 @@
 ## MYSQL
 We need to set up MYSQL before running any programs or setting up the rest of the cron jobs. Look through `readme` to learn how to start/access MySQL. The initialization scripts assume MySQL is already running and `config.json` contains the correct usernames and passwords.
 
+## Setting up MySQL
+
+Run the following:
+
+    venv/bin/python initialize.py 
---
+++ initialize.py
@@ -0,0 +1,22 @@
+# This initializees several things...
+import pdb
+import argparse
+import glob
+import utils
+
+parser = argparse.ArgumentParser(description="Initialize sets up various parts of LITMUS")
+parser.add_argument("--env",
+                    choices=["mysql"],
+                    help="Environment to setup")
+
+argums = vars(parser.parse_args())
+config = utils.load_config()
+
+
+if argums['env'] == 'mysql':
+    #set up mysql stuff (news and everything)
+    db_conn = utils.get_db_connection(config)
+    for file_ in glob.glob('initialization/mysql/*.SQL'):
+        utils.run_sql_file(file_,db_conn)
+        db_conn.close()
+        
