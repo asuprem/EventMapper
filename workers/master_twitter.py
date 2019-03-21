35768797b8a5dbe7420387e20d5f1342f1178146
Abhijit Suprem <asf2182@columbia.edu>
2019-03-21 11:19:57 -0700

    switched to general streamer instead of specifics

--- workers/master_twitter.py
+++ workers/master_twitter.py
@@ -6,7 +6,7 @@ from math import floor
 
 # Multiprocessing import
 import multiprocessing
-from master_twitter_src.MasterProcess import MasterProcess
+from master_twitter_src.TweetProcess import TweetProcess
 from master_twitter_src.KeyServer import KeyServer
 
 # Utils import
@@ -14,14 +14,13 @@ from utils.file_utils import load_config
 from utils.helper_utils import dict_equal, setup_pid, readable_time
 
 CONFIG_PATH = 'config/multiprocess.json'
-CONFIG_TIME_CHECK = 60*60
+CONFIG_TIME_CHECK = 60*2
 
 if __name__ == '__main__':
     pid_name = os.path.basename(sys.argv[0]).split('.')[0]
     setup_pid(pid_name)
     #Set up configOriginal dict
     configOriginal = load_config(CONFIG_PATH)
-    physicalEventTypes = configOriginal['keyws_twitter'].keys()
 
     '''Error queue - This is the queue for errors; Each time process crashes, it will inform this queue'''
     errorQueue = multiprocessing.Queue()
@@ -31,36 +30,94 @@ if __name__ == '__main__':
     keyServer = KeyServer(configOriginal)
 
     
-
-
-    for physicalEvent in physicalEventTypes:
+    '''Launch the Streamer with all keywords'''
+    keywords = []
+    APIKeys = keyServer.get_key()
+    for physicalEvent in configOriginal['keyws_twitter'].keys():
         for language in configOriginal['keyws_twitter'][physicalEvent]:
+            
+            
             eventLangTuple = (physicalEvent,language)
-            print " ".join(["Deploying", physicalEvent,language, "at", readable_time()])
+            #print " ".join(["Deploying", physicalEvent,language, "at", readable_time()])
             streamerConfig[eventLangTuple] = {}
             streamerConfig[eventLangTuple]['name'] = physicalEvent
             streamerConfig[eventLangTuple]['keywords'] = configOriginal['keyws_twitter'][physicalEvent][language]
             streamerConfig[eventLangTuple]['lang'] = language
-            print " ".join(["Initialized",physicalEvent,language , "at", readable_time()])
+            keywords += streamerConfig[eventLangTuple]['keywords']
+            #print " ".join(["Initialized",physicalEvent,language , "at", readable_time()])
             #keys is a tupe (keyStr, key)
+            '''
             streamerConfig[eventLangTuple]['keys'] = keyServer.get_key()
             print " ".join(["Retrieved keys for",physicalEvent,language , "at", readable_time(),"with key", streamerConfig[eventLangTuple]['keys'][0]])
             streamerConfig[eventLangTuple]['process'] = MasterProcess(configOriginal, physicalEvent, language, streamerConfig[eventLangTuple]['keywords'],streamerConfig[eventLangTuple]['keys'][1],errorQueue)
             print " ".join(["Setu up complete for",physicalEvent,language , "at", readable_time()])
             streamerConfig[eventLangTuple]['process'].start()
             print " ".join(["Deployed",physicalEvent,language , "at", readable_time(),"with key", streamerConfig[eventLangTuple]['keys'][0]])
-
-    timer = time.time()
+            '''
+    
+    tweetStreamer = TweetProcess(keywords,APIKeys[1],errorQueue)
+    tweetStreamer.start()
+    print " ".join(["Deployed",'unstructured streamer', "at", readable_time(),"with key", APIKeys[0]])
+    configCheckTimer = time.time()
     while True:
-        if time.time() - timer > CONFIG_TIME_CHECK:
+        if time.time() - configCheckTimer > CONFIG_TIME_CHECK:
 
-            timer = time.time()
+            configCheckTimer = time.time()
             print " ".join(["Checking configuration at", readable_time()])
             configReload = load_config(CONFIG_PATH)
-
+            
+            configChangeFlag = False
             keyServer.update(configReload)
+            #First we check reloaded and for each changed, we replace
+            for physicalEvent in configReload['keyws_twitter'].keys():
+                for language in configReload['keyws_twitter'][physicalEvent]:
+                    eventLangTuple = (physicalEvent,language)
+                    if eventLangTuple not in streamerConfig:
+                        #new pair
+                        streamerConfig[eventLangTuple] = {}
+                        streamerConfig[eventLangTuple]['name'] = physicalEvent
+                        streamerConfig[eventLangTuple]['keywords'] = configReload['keyws_twitter'][physicalEvent][language]
+                        streamerConfig[eventLangTuple]['lang'] = language
+                        if not configChangeFlag:
+                            print "Changes have been made to Multiprocessing config file"
+                            configChangeFlag = True
+                        print "New event-language pair added: ", str(eventLangTuple)
+            deleteEventLangTuples = []
+            for eventLangTuple in streamerConfig:
+                if eventLangTuple[0] not in configReload['keyws_twitter'].keys():
+                    #This event type has been deleted
+                    deleteEventLangTuples.append(eventLangTuple)
+                else:
+                    #Event type exists, but lanuage has been deleted
+                    if eventLangTuple[1] not in configReload['keyws_twitter'][eventLangTuple[0]]:
+                        deleteEventLangTuples.append(eventLangTuple)
+            for eventLangTuple in deleteEventLangTuples:
+                del streamerConfig[eventLangTuple]
+                if not configChangeFlag:
+                    print "Changes have been made to Multiprocessing config file"
+                    configChangeFlag = True
+                print "Deleted event-language pair: ", str(eventLangTuple)
 
-            if not (dict_equal(configOriginal, configReload)):
+            if configChangeFlag:
+                keywords = []
+                for eventLangTuple in streamerConfig:
+                    keywords += streamerConfig[eventLangTuple]['keywords']
+                #relaunch
+                try:
+                    tweetStreamer.terminate()
+                except:
+                    pass
+                tweetStreamer = TweetProcess(keywords,APIKeys[1],errorQueue)
+                tweetStreamer.start()
+                print " ".join(["Deployed",'unstructured streamer', "at", readable_time(),"with key", APIKeys[0]])
+            else:
+                print "No changes have been made to Multiprocessing config file"
+                
+            #Then we iterate through local and for each NOT in, we remove
+
+
+            '''
+            for 
                 print "Changes have been made to Multiprocessing config file"
                 configDifference = {i: configReload[i] for i in set(configReload) - set(configOriginal)}
                 #print (configDifference)
@@ -86,21 +143,32 @@ if __name__ == '__main__':
                 print " ".join(["Finished with Configuration update at", readable_time()])
             else:
                 print "No changes have been made to Multiprocessing config file"
+            '''
 
         if int(floor (time.time() % 120)) < 2:
             print " ".join(["Checking crashes at", readable_time()])
         while not errorQueue.empty():
             
-            eventName, lang, error_ = errorQueue.get()
-            print " ".join([eventName,lang, "crashed with error "]), error_
+            _type, _error = errorQueue.get()
+            print " ".join([_type, "crashed with error "]), error_
             print "        at ", readable_time()
             #TODO TODO TODO have a try catch in case terminate causes problem
+            '''
             streamerConfig[(eventName, lang)]['process'].terminate()
             print " ".join(["Terminating", eventName,lang, "at" , readable_time()])
             streamerConfig[(eventName, lang)]['keys'] = keyServer.refresh_key(streamerConfig[(eventName, lang)]['keys'][0])
             streamerConfig[(eventName, lang)]['process'] = MasterProcess(configOriginal, eventName, lang, streamerConfig[(eventName,lang)]['keywords'], streamerConfig[(eventName,lang)]['keys'][1], errorQueue)
             streamerConfig[(eventName, lang)]['process'].start()
-            print " ".join(["Restarted", eventName,lang, "at" , readable_time()])
+            '''
+            APIKeys = keyServer.refresh_key(APIKeys[0])
+            try:
+                tweetStreamer.terminate()
+            except:
+                pass
+            tweetStreamer = TweetProcess(keywords,APIKeys[1],errorQueue)
+            tweetStreamer.start()
+    
+            print " ".join(["Restarted", _type, "at" , readable_time()])
         time.sleep(5)
 
 
---
+++ workers/master_twitter_src/TweetProcess.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+"""TweetProcess  script
+
+This will launch a twitter streamer for all of twitter
+"""
+
+import multiprocessing
+import time, os, sys, json
+from datetime import datetime
+
+# General tweepy imports
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+from utils.helper_utils import readable_time
+
+class TweetProcess(multiprocessing.Process):
+    """A Class for a process for a multiprocessing application.
+
+    Args:
+        config (dict): A dictionary containing config details. Returned by load_config in file_utils. Config file contains streamer details.
+        name (str): Name of this process. Usually a key field from config file.
+        lang (str): Streamer language. Used for creating output directory structure.
+        keywords (list[str]): List of keywords for generic streamer. Used in Twitter streaming. Planned extensibility to other streamers
+
+    """
+
+    def __init__(self, keywords, apiKeys, errorQueue):
+        #Initializes this class as a child of Multiprocessing.Process
+        multiprocessing.Process.__init__(self)
+
+        """Variable Details
+
+            self.config -           Entire config dict
+            self.timer -            Used to create schedule flushing. Every N seconds, all intermediate data is flushed to disk.
+            self.physical_event -    Name of disaster. Extensible to event_type.
+            self.lang -             Language of streamer downloads
+            self.keywords -         List of streaming keywords for physical event type    
+        
+        """
+        self.timer = time.time()
+        self.keywords = keywords
+        self.errorQueue = errorQueue
+
+
+        """ The next four are API keys.
+            TODO --> Change to streamer specific
+
+            self.auth are the OAuth Handlers for API keys for Tweepy; TODO --> Modify to be streamer specific
+            self.stream is the Stream function. TODO --> Modify to be streamer specific
+
+            self.time - current time
+        """            
+
+        self.access_token_secret = apiKeys[0]
+        self.access_token = apiKeys[1]
+        self.consumer_key = apiKeys[2]
+        self.consumer_secret = apiKeys[3]
+
+        self.auth = OAuthHandler(self.consumer_key, self.consumer_secret)
+        self.auth.set_access_token(self.access_token, self.access_token_secret)
+        self.stream = Stream(self.auth, self.tweetStreamer())
+
+    def run(self):
+        """Run - Launches the sreamer itself.
+
+        """
+        # TODO change print to logging; use logging function/API
+        try:
+            #print "entered" + " " + self.physical_event + " "+ self.lang
+            #TODO have changed from json_array to self.keywords
+            #json_array = config['keyws_twitter'][self.physical_event][self.lang]
+            #print json_array
+            self.stream.filter(track=self.keywords)
+            print " ".join(["Running unstructured streamer", "with PID", str(os.getpid()), "at", readable_time()])
+            pid = str(os.getpid())
+        except Exception as e:
+            print '------------'
+            print " ".join(["Crashed unstructured stream", "at", readable_time()])
+            print e
+            print '------------'
+            #TODO TODO TODO CHECK THIS
+            self.errorQueue.put(('unstructured', e))
+
+
+
+    class tweetStreamer(StreamListener):
+        """A Class for a Streamer for a multiprocess application.
+
+        Args:
+            physical_event (str):    A string with the name of the physical_event. Used for .
+            lang (str): Language for this physical event social streamer
+
+        """
+        def __init__(self,):
+            """Variables
+
+                    TIMER (const int)       This is a scheduler. It is the number of seconds between scheduled file flushes to disk.
+                    local_time (time)       Time for the streamer. Use to schedule disk flushes
+                    date (time)             Current date. Used for flush directory (each download is sent to a day/hour/minute folder)
+                    physical_event (str)     The name of physical event. used to create top level name for flush folder
+                    lang (str)              Language of this physical event streamer. Used to create top level nanme for flush folder
+                    output (PATH)           This is the flush path. Each data item is written to this file buffer.
+
+            """
+            self.TIMER = 60
+            self.local_time = time.time()
+            self.date = datetime.utcnow()
+            self.PREPEND = './downloads/'
+
+            #Creates flush path
+            self.dir = os.path.join(self.PREPEND + '%s_%s_%s' % ('tweets', 'unstructured', self.date.year), '%02d' % self.date.month,
+                                    '%02d' % self.date.day, '%02d' % self.date.hour)
+            self.path = os.path.join(self.dir, '%02d.json' % self.date.minute)
+            
+            #Error checking for directory create
+            try:
+                os.makedirs(self.dir)
+            except:
+                pass
+
+            #Create write path
+            self.output = open(self.path, 'w')
+
+
+        def on_data(self, data):
+            """on_data - This function is an interface in Tweepy's streamers. It is activated on each data item"""
+
+            """ Operation
+
+            Each time it is activated, on_data checks if TIMER has been surpassed. If it has, on_data creates a new file with the current time (because on_data flushes every TIMER seconds). This new file is what on_data operates on.
+
+            If TIMER has not been surpassed, on_data writes data to self.output, which is the flush path
+            """
+            try:
+                #check TIMER
+                if time.time() - self.local_time > self.TIMER:
+                    self.path_setup()
+                sample = json.loads(data)
+                #get rid of retweets and keep only source tweets
+                if sample['in_reply_to_user_id'] is not None or 'retweeted_status' in sample:
+                    pass
+                else:
+                    self.output.write(data)
+            except:
+                pass
+
+        def path_setup(self):
+            """path_setup()
+
+            Function to set up a path.
+            """
+            self.local_time = time.time()
+            self.date = datetime.utcnow()
+            self.dir = os.path.join(self.PREPEND+'%s_%s_%s' % ('tweets', 'unstructured', self.date.year),
+                                    '%02d' % self.date.month,
+                                    '%02d' % self.date.day, '%02d' % self.date.hour)
+            self.path = os.path.join(self.dir, '%02d.json' % self.date.minute)
+            try:
+                os.makedirs(self.dir)
+            except:
+                pass
+            self.output.close()
+            self.output = open(self.path, 'w')
