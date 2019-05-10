15ce11ab6b85f0926b55c315de1ccd0383b5147c
Abhijit Suprem <asuprem@gatech.edu>
2019-05-10 16:54:27 -0700

    added input script generator

--- utils/AssedPipeline.py
+++ utils/AssedPipeline.py
@@ -10,8 +10,9 @@ class AssedPipeline():
         self.config = pipeline_config
         self.home_dir = home_dir
 
-        # Create Log directory
+        # Create Log directory TODO Sanitize
         self.log_dir = "./logfiles/" + self.config["configuration"]["log_dir"]
+        self.script_dir = "./pipelines/" + self.config["configuration"]["script_dir"]
         if not os.path.exists(self.log_dir):
             helper_utils.std_flush("Log directory not created. Creating")
             os.makedirs(self.log_dir)
@@ -19,8 +20,35 @@ class AssedPipeline():
         helper_utils.std_flush("Finished verifying log directory %s"%self.log_dir)
 
         # Create Scripts
-        pass
+        self.createInputBufferScript()
+        self.createProcessScripts()
+        self.createOutputBufferScript()
         
+    def createInputBufferScript(self):
+        scriptname = self.config["input_buffer"]["script"]
+        exportkey = self.config["input_buffer"]["export-key"]
+        bufferStr = \
+        '''#!/bin/sh
+        cd {homedir}
+        if ps up `cat {logdir}/{inputbuffername}.pid ` > /dev/null
+        then
+            printf "{inputbuffername}.py is aleady running\n" >> {logdir}/{inputbuffername}.out
+        else
+            printf "{inputbuffername} is no longer running. Deleting PID file.\n" >> {logdir}/{inputbuffername}.out
+            rm  {logdir}/{inputbuffername}.pid >> {logdir}/{inputbuffername}.out
+            printf "Deleted file\n" >> {logdir}/{inputbuffername}.out
+            printf "Starting {inputbuffername}.py\n" >> {logdir}/{inputbuffername}.out
+            nohup ./assed_env/bin/python {scriptdir}/{inputbuffername}.py {exportkey} >> {logdir}/{inputbuffername}.log 2>&1 &
+        fi'''.format(homedir = self.home_dir, logdir = self.log_dir, inputbuffername = scriptname, scriptdir = self.script_dir, exportkey = exportkey)
+        
+        pdb.set_trace()
+
+    def createProcessScripts(self):
+        pass
+
+    def createOutputBufferSscript(self):
+        pass    
+    
     def run(self,):
         # Initiate each of the config executables.
         # Assume they all require python and are executed through assed_env
