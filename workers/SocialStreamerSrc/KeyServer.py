fdc49e7b60df260401b8a2002a09f1d3ed669fb9
Abhijit Suprem <asf2182@columbia.edu>
2019-05-09 22:38:24 -0700

    Updated keyserver to deal with assed_env instead of multiprocess.json

--- workers/SocialStreamerSrc/KeyServer.py
+++ workers/SocialStreamerSrc/KeyServer.py
@@ -3,66 +3,86 @@
 
 class KeyServer():
 
-    def __init__(self,config):
-        #Set up keyserver
+    def __init__(self,config, key_mode="twitter", key_max = 1):
+        """ Initialize the KeyServer.
+
+        Takes in parameters to set up the key distribution server
+
+        Args:
+            config -- [DICT] The entire assed_config.json dict. Normally passed by using load_config('assed_config.json')
+            key_mode -- which application's keys to use
+            key_max -- maximum number of assignments per key. This is for applications that need to use the same key for multiple applications. Sometimes API sources limit number of concurrent connections per key.
+
+        """
+ 
         self.keyserver={}
-        for _key in config["APIKeys"]["keys"]:
+        self.key_max = key_max
+        self.key_mode = key_mode
+        for _key in config["APIKEYS"][self.key_mode]:
             self.keyserver[_key] = {}
-            self.keyserver[_key]["key"] =  config["APIKeys"]["keys"][_key]
+            self.keyserver[_key]["key"] =  config["APIKEYS"][self.key_mode][_key]
             self.keyserver[_key]["count"] = 0
             self.keyserver[_key]["valid"] = True
-            self.keyserver[_key]["MAX"] = 2
 
-    #Compare to config and update keyserver
     def update(self,config):
-        for _key in config["APIKeys"]["keys"]:
-            #new key in config that isn't in keyserver
+        """ Update keyserver with new configuration file """
+
+        for _key in config["APIKeys"][self.key_mode]:
+            # For a new key in config that is not in key_server
             if _key not in self.keyserver:
                 self.keyserver[_key] = {}
-                self.keyserver[_key]["key"] =  config["APIKeys"]["keys"][_key]
+                self.keyserver[_key]["key"] =  config["APIKEYS"][self.key_mode][_key]
                 self.keyserver[_key]["count"] = 0
                 self.keyserver[_key]["valid"] = True
-                self.keyserver[_key]["MAX"] = 2
             else:
-                #if key exists in keyserver - check if info has changed
-                #TODO generic
+                # Key exists in keyserver, but may have changed...
                 if self.keyserver[_key]["key"] != config["APIKeys"]["keys"][_key]:
                     self.keyserver[_key]["key"] = config["APIKeys"]["keys"][_key]
-        #Now all keys in config have been validated. So any key in keyserver that is not in keyserver is a deleted key
-        #We invalidate it
+        # Now all keys in config have been validated.Any remaining key in keyserver not in config is a deleted key. Invalidate the key
         for _key in self.keyserver:
-            if _key not in config["APIKeys"]["keys"]:
+            if _key not in config["APIKEYS"][self.key_mode]:
                 self.invalidate(_key)
         
-    #get a new key
     def get_key(self,):
+        """ Get the first valid key (or tuple or what have you), with its keyname """
         for _key in self.keyserver:
-            if self.keyserver[_key]["count"] < self.keyserver[_key]["MAX"] and self.keyserver[_key]["valid"]:
+            if self.keyserver[_key]["count"] <= self.key_max and self.keyserver[_key]["valid"]:
                 self.keyserver[_key]["count"]+=1
                 return (_key, self.keyserver[_key]["key"])
-        raise(self.NotEnoughKeys)
+        raise(IndexError("Not enough keys available in KeyServer for key_mode='%s'"%self.key_mode))
 
-    #refresh an existing key
     def refresh_key(self,_key):
-        if _key in self.keyserver:
-            if self.keyserver[_key]["valid"]:
-                return (_key, self.keyserver[_key]["key"])
-            else:
-                self.keyserver[_key]["count"]-=1
-                self.verify()
-        #either key is not in key_server - something funky happened, ot is was but it was invalid
+        """ Refresh an existing key (i.e. if it has been changed, etc etc)
+        
+        Args:
+            _key -- key_name provided during get_key()
+        
+        Raises:
+            ValueError if _key not in KeyServer, which shouldn't happen unless thing go really really awry.
+
+        """
+        # The _key is still valid
+        if self.keyserver[_key]["valid"]:
+            return (_key, self.keyserver[_key]["key"])
+        else:
+            # The _key is no longer still valid
+            self.keyserver[_key]["count"]-=1
+            self.verify()
+        # Return a new key_tuple pair.
         return self.get_key()
         
-    #Verify -- remove invalid keys with zero count
     def verify(self,):
+        """ Verify -- remove invalid keys with zero count """
         for _key in self.keyserver:
             if not self.keyserver[_key]["valid"]:
-                #TODO check with refresh key for less than 0 valid check - at no point should key count be less than 0
-                if self.keyserver[_key]["count"]<=0:
+                if self.keyserver[_key]["count"] == 0:
                     del self.keyserver[_key]
+                elif self.keyserver[_key]["count"] < 0:
+                    raise ValueError("Key count is less than 0.")
+                else:
+                    pass
 
     def invalidate(self,_key):
-        self.keyserver[_key]["valid"] = False
+        """ Invalidate a key and schedule it for deletion. """
 
-    class NotEnoughKeys(Exception):
-        pass
+        self.keyserver[_key]["valid"] = False
