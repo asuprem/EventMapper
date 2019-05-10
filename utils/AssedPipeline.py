3553ed90848686798be65090567f3674ae405eb7
Abhijit Suprem <asuprem@gatech.edu>
2019-05-10 15:06:54 -0700

    fixed bug

--- utils/AssedPipeline.py
+++ utils/AssedPipeline.py
@@ -7,9 +7,7 @@ class AssedPipeline():
     def __init__(self,pipeline_config):
         self.config = pipeline_config
 
-
-
-        def run(self,):
-            # Initiate each of the config executables.
-            # Assume they all require python and are executed through assed_env
-            pdb.set_trace()
+    def run(self,):
+        # Initiate each of the config executables.
+        # Assume they all require python and are executed through assed_env
+        pdb.set_trace()
