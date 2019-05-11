8291302b1faffccd8f4ba8dc8ad2d3b34510ed9e
Abhijit Suprem <asuprem@gatech.edu>
2019-05-11 11:11:17 -0700

    added inits and assedmessageprocessor child

---
+++ pipelines/assed_landslide/landslide_location_extractor.py
@@ -0,0 +1,7 @@
+import utils.AssedMessageProcessor
+
+class landslide_location_extractor(utils.AssedMessageProcessor.AssedMessageProcessor):
+
+    def process(self,message):
+        import pdb
+        pdb.set_trace()
