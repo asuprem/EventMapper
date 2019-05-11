2ea5670d3f1a889c0096d5dd4f769b6f49702134
Abhijit Suprem <asuprem@gatech.edu>
2019-05-11 14:02:52 -0700

    location extraction

--- pipelines/assed_landslide/landslide_location_extractor.py
+++ pipelines/assed_landslide/landslide_location_extractor.py
@@ -3,6 +3,8 @@ import time, redis
 import pdb
 from sner import Ner
 import nltk
+import utils.helper_utils
+
 class landslide_location_extractor(utils.AssedMessageProcessor.AssedMessageProcessor):
     def __init__(self):
         self.time = time.time()
@@ -12,6 +14,7 @@ class landslide_location_extractor(utils.AssedMessageProcessor.AssedMessageProce
         self.locations = {}
         self.update_location_store()
         self.NER =  Ner(host="localhost", port=9199)
+        self.counter = 0
 
     def process(self,message):
         if time.time() - self.time > self.timecheck:
@@ -19,10 +22,45 @@ class landslide_location_extractor(utils.AssedMessageProcessor.AssedMessageProce
         
         # First location tagging to get locations...
         pdb.set_trace()
-        cleaned_message = " ".join(nltk.tokenize.word_tokenize(message["text"].encode("utf-8")))
+        cleaned_message = str(message["text"].encode("utf-8"))[2:-2]
+        cleaned_message = " ".join(nltk.tokenize.word_tokenize(cleaned_message))
         loc_tags = self.NER.get_entities(cleaned_message)
         desc_locations = self.extractLocations(loc_tags)
-        pdb.set_trace()
+        locations = " ".join(desc_locations) if len(desc_locations) > 0 else None
+        latitude = None
+        longitude = None
+
+        if locations is None:
+            # Attempt match...
+            for sublocations in self.locations:
+                if sublocations in cleaned_message:
+                    locations = sublocations
+                    latitude = self.locations[sublocations][0]
+                    longitude = self.locations[sublocations][1]
+        else:
+            self.counter+=1
+            utils.helper_utils.std_flush(self.counter)
+                    
+        if locations is None:
+            return (False, message)
+
+        # location is there, we will attempt geocoding right here... right now... right on this ship
+        # With sublocations...
+        if latitude is None or longitude is None:
+            standardized_location = utils.helper_utils.location_standardize(locations)
+
+            for sublocation in standardized_location.split(":"):
+                if sublocation in self.locations:
+                    latitude = self.locations[sublocation][0]
+                    longitude = self.locations[sublocation][1]
+        
+        message["locatioin"] = locations
+        if latitude is not None and longitude is not None:
+            message["latitude"] = str(latitude)
+            message["longitude"] = str(longitude)
+
+
+        return (True, message)
         
 
 
