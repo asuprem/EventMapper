592c41113041ffc1908a4e09134d835a5fd0f270
Abhijit Suprem <asuprem@gatech.edu>
2019-05-11 11:54:02 -0700

    added redis code to loc ext

--- pipelines/assed_landslide/landslide_location_extractor.py
+++ pipelines/assed_landslide/landslide_location_extractor.py
@@ -1,7 +1,19 @@
 import utils.AssedMessageProcessor
+import time, redis
 
 class landslide_location_extractor(utils.AssedMessageProcessor.AssedMessageProcessor):
+    def __init__(self):
+        self.time = time.time()
+        pool = redis.ConnectionPool(host='localhost',port=6379, db=0)
+        self.r=redis.Redis(connection_pool = pool)
+        self.timecheck = 7200
+        self.locations = []
+        self.update_location_store()
 
     def process(self,message):
         import pdb
+        pdb.set_trace()
+
+
+    def update_location_store(self,)
         pdb.set_trace()
