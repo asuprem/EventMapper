fb934c2018ad04966b4ca3cc7eabd3590333ce92
Abhijit Suprem <asuprem@gatech.edu>
2019-05-11 14:14:21 -0700

    debugging

--- pipelines/assed_process.py
+++ pipelines/assed_process.py
@@ -20,8 +20,7 @@ def main(logdir, importkey, exportkey, processscript, processscriptdir):
     #helper_utils.setup_pid(pid_name, logdir=logdir)
 
     # Import processscript
-    import pipelines.assed_landslide.landslide_location_extractor as locext
-    pdb.set_trace()
+
     moduleImport = __import__("pipelines.%s.%s"%(processscriptdir, processscript), fromlist=[processscript])
     MessageProcessor = getattr(moduleImport, processscript)
     MessageProcessor = MessageProcessor()
