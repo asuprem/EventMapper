e71d4406a0704f9c8095545db48afe4d0da19463
Abhijit Suprem <asuprem@gatech.edu>
2019-05-10 17:15:15 -0700

    added basic input buffer script

--- pipelines/assed_landslide/landslide_inputbuffer.py
+++ pipelines/assed_landslide/landslide_inputbuffer.py
@@ -1,8 +1,16 @@
+import click
+import utils.helper_utils as helper_utils
+import time
+
+@click.command()
+@click.argument("importkey")
+def main(importkey):
+    helper_utils.std_flush(importkey)
+
+    while True:
+        time.sleep(10)
+        helper_utils.std_flush(importkey)
 
 
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()  #pylint: disable=no-value-for-parameter
