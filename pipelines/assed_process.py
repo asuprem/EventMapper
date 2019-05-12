ebaeb4296d4805ff4021f315b9f214a81ddd6d95
Abhijit Suprem <asuprem@gatech.edu>
2019-05-11 17:19:42 -0700

    added comments

--- pipelines/assed_process.py
+++ pipelines/assed_process.py
@@ -21,26 +21,34 @@ def main(logdir, importkey, exportkey, processscript, processscriptdir, pidname)
     #helper_utils.setup_pid(pid_name, logdir=logdir)
 
     # Import processscript
-
+    helper_utils.std_flush("Initializing ASSED-Process %s"%pidname)
     moduleImport = __import__("pipelines.%s.%s"%(processscriptdir, processscript), fromlist=[processscript])
     MessageProcessor = getattr(moduleImport, processscript)
     MessageProcessor = MessageProcessor()
+    helper_utils.std_flush("Imported Module %s"%processscript)
     
     kafka_import = importkey.replace(":","_")
+    helper_utils.std_flush("Generated kafka import key %s"%kafka_import)
     kafka_export = exportkey.replace(":","_")
+    helper_utils.std_flush("Generated kafka export key %s"%kafka_export)
     pool = redis.ConnectionPool(host='localhost',port=6379, db=0)
     r=redis.Redis(connection_pool = pool)
+    helper_utils.std_flush("Connected to redis")
     
     seek_partition = r.get(exportkey+":partition")
     seek_offset = r.get(exportkey+":offset")
     seek_partition = 0 if seek_partition is None else int(seek_partition)
     seek_offset = 0 if seek_offset is None else int(seek_offset)+1
+    helper_utils.std_flush("Obtained seek partition for kafka at Partition %i -- Offset %i"%(seek_partition, seek_offset))
     
     kafka_producer = kafka.KafkaProducer()
+    helper_utils.std_flush("Generated kafka producer")
     kafka_consumer = kafka.KafkaConsumer()
+    helper_utils.std_flush("Generated kafka consumer")
     TopicPartition = kafka.TopicPartition(kafka_import, seek_partition)
     kafka_consumer.assign([TopicPartition])
     kafka_consumer.seek(TopicPartition, seek_offset)
+    helper_utils.std_flush("Set kafka consumer seek")
     
     for message in kafka_consumer:
         item = json.loads(message.value.decode())
