7b0ff9d3739920e8ed4fc187fca0432931044aa2
Abhijit Suprem <asuprem@gatech.edu>
2019-05-11 22:06:32 -0700

    initialized hdi

---
+++ pipelines/assed_landslide/landslide_hdi.py
@@ -0,0 +1,14 @@
+import utils.AssedMessageProcessor
+import pdb
+
+class landslide_hdi(utils.AssedMessageProcessor.AssedMessageProcessor):
+
+    def __init__(self):
+
+
+        pass
+
+
+    def process(self,message):
+        pass
+        pdb.set_trace()
