5033592f72740dcb4f7a068a913fea1d0c5ff0b4
Abhijit Suprem <asuprem@gatech.edu>
2019-05-11 18:54:41 -0700

    bug fix

--- pipelines/assed_process.py
+++ pipelines/assed_process.py
@@ -61,7 +61,7 @@ def main(logdir, importkey, exportkey, processscript, processscriptdir, pidname)
             pass
             # Message succeeded. We will push to kafka.
             #helper_utils.std_flush("%s failed to parse item with id: %s"%(processscript, item["id_str"]))
-            helper_utils.std_flush(processedMessage[1])
+            helper_utils.std_flush(str(processedMessage[1]))
             time.sleep(3)
             #byted = bytes(json.dumps(processedMessage[1]), encoding="utf-8")
             #kafka_producer.send(kafka_export, byted)
