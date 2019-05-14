12daa7d807215539b84a7cc7dae6e22bee35c5c0
Abhijit Suprem <asuprem@gatech.edu>
2019-05-13 23:19:10 -0700

    upgraded buffer scripts

--- utils/AssedPipeline.py
+++ utils/AssedPipeline.py
@@ -24,13 +24,21 @@ class AssedPipeline():
         self.createIfNotExists(self.script_dir)
         self.createIfNotExists(self.sh_dir)
         
+        # Identify input buffer scripts
+        self.inverted_buffer_index = {}
+        for stream_input_source in self.config["configuration"]["input-streams"]:
+            _bufferscript = self.config["input-streams"][stream_input_source]["buffer-script-name"]
+            if _bufferscript not in self.inverted_buffer_index:
+                self.inverted_buffer_index[_bufferscript] = []
+            self.inverted_buffer_index[_bufferscript].append(stream_input_source)
+
         # Script files
         self.inputBufferScriptFile = None
         self.outputBufferScriptFile = None
         self.processScripts = []
 
         # set up script deets
-        self.input_scripts = ['input_buffer']
+        self.input_scripts = [item for item in self.inverted_buffer_index]
         self.output_scripts = ['output_buffer']
         self.process_scripts = []
         for script_type in self.config:
@@ -73,26 +81,33 @@ class AssedPipeline():
             r.delete(r_timestamp)
 
     def createInputBufferScript(self):
-        for _inputscript in self.input_scripts:
-            scriptname = self.config[_inputscript]["script"]
-            inputbuffername = self.config[_inputscript]["name"]
-            exportkey = self.config[_inputscript]["export-key"]
+        # For each input script type (text, or image, wher we get to it...)
+        for _bufferscript in self.input_scripts:
+            # get the input streams for this...
+            for _inputsource in self.inverted_buffer_index[_bufferscript]:
+                bufferscriptname = self.config[_bufferscript]["script"]
+                bufferlogname = self.config["configuration"]["input-streams"][_inputsource]["name"]
+                exportkey = self.config[_bufferscript]["export-key"]
+                dataprocessor = self.config["configuration"]["input-streams"][_inputsource]["processor_script"]
+                importkey = self.config["configuration"]["input-streams"][_inputsource]["import-key"]
+
+            
             bufferStr = \
             '''#!/bin/sh
 cd {homedir}
-if ps up `cat {logdir}/{inputbuffername}.pid ` > /dev/null
+if ps up `cat {logdir}/{bufferlogname}.pid ` > /dev/null
 then
-    printf "{inputbufferscriptname}.py is aleady running\\n" >> {logdir}/{inputbuffername}.out
+    printf "{bufferscriptname}.py is aleady running\\n" >> {logdir}/{bufferlogname}.out
 else
-    printf "{inputbuffername} is no longer running. Deleting PID file.\\n" >> {logdir}/{inputbuffername}.out
-    rm  {logdir}/{inputbuffername}.pid >> {logdir}/{inputbuffername}.out
-    printf "Deleted file\\n" >> {logdir}/{inputbuffername}.out
-    printf "Starting {inputbuffername}.py\\n" >> {logdir}/{inputbuffername}.out
-    nohup ./assed_env/bin/python {scriptdir}/{inputbufferscriptname}.py {logdir} {exportkey} {pidname} >> {logdir}/{inputbuffername}.log 2>&1 &
-fi'''.format(homedir = self.home_dir, logdir = self.log_dir, inputbufferscriptname = scriptname, inputbuffername = inputbuffername, scriptdir = self.script_dir, exportkey = exportkey, pidname=inputbuffername)
+    printf "{bufferlogname} is no longer running. Deleting PID file.\\n" >> {logdir}/{bufferlogname}.out
+    rm  {logdir}/{bufferlogname}.pid >> {logdir}/{bufferlogname}.out
+    printf "Deleted file\\n" >> {logdir}/{bufferlogname}.out
+    printf "Starting {bufferscriptname}.py\\n" >> {logdir}/{bufferlogname}.out
+    nohup ./assed_env/bin/python {assedscript}/{bufferscriptname}.py {logdir} {importkey} {exportkey} {dataprocessor} {dataprocessorscriptdir} {pidname} >> {logdir}/{bufferlogname}.log 2>&1 &
+fi'''.format(homedir = self.home_dir, logdir = self.log_dir, bufferscriptname = bufferscriptname, bufferlogname = bufferlogname, assedscript = self.assed_sript_dir, importkey = importkey, exportkey = exportkey, dataprocessor = dataprocessor, pidname=bufferlogname)
         
 
-        self.inputBufferScriptFile = os.path.join(self.sh_dir, scriptname + ".sh")
+        self.inputBufferScriptFile = os.path.join(self.sh_dir, bufferlogname + ".sh")
         self.writeScript(self.inputBufferScriptFile, bufferStr)
         helper_utils.std_flush("Generated script for Input Buffer at %s"%self.inputBufferScriptFile)
 
