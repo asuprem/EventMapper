8c14bd239fa905218e90f3fb903768e740b70509
Abhijit Suprem <asuprem@gatech.edu>
2019-05-14 08:08:44 -0700

    added assed debugger

---
+++ pipelines/assed_debugger.py
@@ -0,0 +1,36 @@
+import sys, os
+sys.path.append(os.getcwd())
+
+import kafka, redis
+import pdb, click
+import utils.helper_utils as helper_utils
+
+@click.command()
+@click.argument("importkey")
+@click.argument("exportkey")
+def main(importkey, exportkey):
+
+    kafka_import = importkey.replace(":","_")
+    helper_utils.std_flush("Generated kafka import key %s"%kafka_import)
+    kafka_export = exportkey.replace(":","_")
+    helper_utils.std_flush("Generated kafka export key %s"%kafka_export)
+    pool = redis.ConnectionPool(host='localhost',port=6379, db=0)
+    r=redis.Redis(connection_pool = pool)
+    helper_utils.std_flush("Connected to redis")
+
+    seek_partition = r.get(exportkey+":partition")
+    seek_offset = r.get(exportkey+":offset")
+    seek_partition = 0 if seek_partition is None else int(seek_partition)
+    #seek_offset = 0 if seek_offset is None else int(seek_offset)+1
+    seek_offset = 0
+    helper_utils.std_flush("Obtained seek partition for kafka at Partition %i -- Offset %i"%(seek_partition, seek_offset))
+
+    kafka_consumer = kafka.KafkaConsumer()
+    helper_utils.std_flush("Generated kafka consumer")
+    TopicPartition = kafka.TopicPartition(kafka_import, seek_partition)
+    kafka_consumer.assign([TopicPartition])
+    kafka_consumer.seek(TopicPartition, seek_offset)
+    helper_utils.std_flush("Set kafka consumer seek")
+
+    for message in kafka_consumer:
+        pdb.set_trace()
