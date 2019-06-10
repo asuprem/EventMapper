73c14102368a24e4355332616d77efc7b5085e39
Abhijit <asuprem@gatech.edu>
2019-06-10 15:28:24 -0300

    initial packaging

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='assed',
+      version='0.1',
+      description='ASSED - Adaptive Social Sensor Event Detection',
+      url='http://github.com/asuprem/ASSED',
+      author='Abhijit Suprem',
+      author_email='asuprem@gatech.edu',
+      license='MIT',
+      packages=['assed'],
+      install_requires=[
+          'flask','click', 'cmd2'
+      ],
+      python_requires=">=3.5",
+      scripts=["bin/assed-cli", "bin/assed-engine", "bin/assed-server", "bin/assed-sh"],
+      zip_safe=False)
