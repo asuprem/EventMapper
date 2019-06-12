00690b111465803a53186569ecbf509e52ddcf6b
Abhijit <asuprem@gatech.edu>
2019-06-12 10:39:15 -0300

    removed installers

--- activate.sh
+++ activate.sh
@@ -11,29 +11,29 @@ else
 
     echo "$(date +"%T") -- Activating virtual environment assed_env"
     source ./assed_env/bin/activate
-
-    echo "$(date +"%T") -- Beginning package installs."
-    echo "$(date +"%T") -- Installing basic packages"
-    # Basics
-    pip3 install numpy scipy keras tensorflow 
-    echo "$(date +"%T") -- Installing database packages"
-    # Database/Interconnects
-    pip3 install redis mysqlclient kafka-python
-    echo "$(date +"%T") -- Installing language tools"
-    # Language tools
-    pip3 install gensim nltk sner
-    echo "$(date +"%T") -- Installing interface utilities"
-    # Interface utilities
-    pip3 install click cmd2 
-    echo "$(date +"%T") -- Installing streamer utilities"
-    # Streamer utilities
-    pip3 install newsapi-python tweepy 
-    echo "$(date +"%T") -- Installing web utilities"
-    # Web utilities
-    pip3 install flask requests
-    echo "$(date +"%T") -- Downloading nltk language tools"
-    # Need to download nltk language tool
-    python -c "import nltk; nltk.download('punkt')"
+    
+    # echo "$(date +"%T") -- Beginning package installs."
+    # echo "$(date +"%T") -- Installing basic packages"
+    # # Basics
+    # pip3 install numpy scipy keras tensorflow 
+    # echo "$(date +"%T") -- Installing database packages"
+    # # Database/Interconnects
+    # pip3 install redis mysqlclient kafka-python
+    # echo "$(date +"%T") -- Installing language tools"
+    # # Language tools
+    # pip3 install gensim nltk sner
+    # echo "$(date +"%T") -- Installing interface utilities"
+    # # Interface utilities
+    # pip3 install click cmd2 
+    # echo "$(date +"%T") -- Installing streamer utilities"
+    # # Streamer utilities
+    # pip3 install newsapi-python tweepy 
+    # echo "$(date +"%T") -- Installing web utilities"
+    # # Web utilities
+    # pip3 install flask requests
+    # echo "$(date +"%T") -- Downloading nltk language tools"
+    # # Need to download nltk language tool
+    # python -c "import nltk; nltk.download('punkt')"
 
     echo "$(date +"%T") -- Finished."
 fi
