db30c7f7bf9253cef1ae45b383c945e1d7b785c2
Abhijit <asuprem@gatech.edu>
2019-06-12 10:42:44 -0300

    debugging

--- activate.sh
+++ activate.sh
@@ -9,10 +9,10 @@ else
     echo "$(date +"%T") -- Generating virtual environment assed_env"
     virtualenv -p python3.6 assed_env
 
-    which pip | echo
+    which pip
     echo "$(date +"%T") -- Activating virtual environment assed_env"
     source ./assed_env/bin/activate
-    which pip | echo
+    which pip
     # echo "$(date +"%T") -- Beginning package installs."
     # echo "$(date +"%T") -- Installing basic packages"
     # # Basics
